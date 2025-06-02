faa32b8f9caf16d7435250ff77dcf82cdba1d21d
Soham <sohamholkar800@gmail.com>
2025-06-02 18:45:43 +0530

    Solved the profile error

--- src/Pages/RanksPage.jsx
+++ src/Pages/RanksPage.jsx
@@ -39,11 +39,11 @@ const UserRankings = () => {
   };
 
   return (
-    <div className="p-4 sm:p-8 max-w-5xl mx-auto">
-      <h2 className="text-2xl font-bold text-center mb-6">🏆 User Rankings</h2>
+    <div className="p-4 sm:p-8 max-w-5xl mx-auto dark:bg-black" >
+      <h2 className="text-2xl font-bold text-center mb-6 dark:bg-black dark:text-white">🏆 User Rankings</h2>
       <div className="overflow-x-auto">
         <table className="min-w-full border border-gray-300 text-sm sm:text-base">
-          <thead className="bg-gray-200">
+          <thead className="bg-gray-200 dark:text-white">
             <tr>
               <th className="px-4 py-2">Rank</th>
               <th className="px-4 py-2">GfG Profile</th>
